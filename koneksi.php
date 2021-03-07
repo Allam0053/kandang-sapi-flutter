3850e7a371981e526b0380fbdfa148bfd8d3d50e
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-03-07 22:23:43 +0700

    icon

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+    $connect = new mysqli("localhost","root", "", "db_berita");
+    if ($connect) {
+    // echo "database connected";
+    
+    } else {
+    // echo "database error";
+    }
+?>
